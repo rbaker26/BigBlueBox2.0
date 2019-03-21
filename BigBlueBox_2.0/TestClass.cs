d13a994132397d31878f63e9fe98125d3da682a9
Bob Baker <007dsi@gmail.com>
2019-03-20 21:17:14 -0700

    Add files via upload

---
+++ BigBlueBox_2.0/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBlueBox_2._0
+{
+    public class TestClass
+    {
+        public TestClass()
+        {
+            Console.Out.WriteLine("Hello world");
+        }
+
+        public void Print()
+        {
+            Console.Out.WriteLine("Hello world222");
+        }
+    }
+}
