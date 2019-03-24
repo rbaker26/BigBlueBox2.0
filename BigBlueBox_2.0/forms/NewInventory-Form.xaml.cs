693e9ff68c6404bc3274196289f71a0493cf9faf
burgerlazer <32382766+TheCynicalBadger@users.noreply.github.com>
2019-03-24 14:33:38 -0700

    Inventory forms added (im inefficient with my commits)

---
+++ BigBlueBox_2.0/forms/NewInventory-Form.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BigBlueBox_2._0.forms
+{
+    /// <summary>
+    /// Interaction logic for NewInventory_Form.xaml
+    /// </summary>
+    public partial class NewInventory_Form : Page
+    {
+        public NewInventory_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
